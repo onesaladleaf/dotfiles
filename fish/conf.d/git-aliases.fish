e46d29957f53595f27687169a13a0f98803ab588
onesaladleaf <180638599+onesaladleaf@users.noreply.github.com>
2026-04-10 21:15:51 +0300

    fish: git aliases, toolbx alias, toolbx container name in the fish prompt

---
+++ fish/conf.d/git-aliases.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    alias gst="git status"
+    alias gts="git status"
+    alias ga="git add"
+    alias gc="git commit"
+    alias gcm="git commit -m"
+    alias gd="git diff"
+end
--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,6 @@
 if status is-interactive
     alias run0="run0 --background="
     alias runo="run0 --background="
-    alias gst="git status"
-    alias gts="git status"
     alias py="python3"
+    alias dev="toolbox enter dev"
 end
